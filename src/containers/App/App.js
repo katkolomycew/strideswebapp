cc3853f1d85c0b82801adb19e23eae8df1f07f1b
Kat Kolomycew <kat.kolomycew@nology.io>
2021-02-05 13:35:00 +0000

    Creating initial navigation and preparing for future components

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "../Home/Home";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>test</h1>
+        <Router>
+          <Switch>
+            <Route path="/" component={Home} />
+            <Route path="/signup" />
+            <Route path="/login" />
+          </Switch>
+        </Router>
       </div>
     );
   }
---
+++ src/containers/Home/Home.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { Button } from "react-bootstrap";
+import Logo from "../../components/Logo/Logo";
+import "./Home.css";
+
+class Home extends Component {
+  onSignup = () => this.props.history.push("/signup");
+  onLogin = () => this.props.history.push("/login");
+
+  render() {
+    return (
+      <div id="home_wrapper">
+        <Logo />
+        <div className="mb-2 centered-button home-button">
+          <Button variant="primary" size="lg" onClick={this.onSignup}>
+            Sign Up
+          </Button>
+        </div>
+        <div className="mb-2 centered-button home-button">
+          <Button variant="primary" size="sm" onClick={this.onLogin}>
+            Already Have an Account?
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
