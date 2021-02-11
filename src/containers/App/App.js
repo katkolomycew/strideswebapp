54851de9606a79bb856c65f11186024b02835645
Kat Kolomycew <kat.kolomycew@nology.io>
2021-02-11 11:55:25 +0000

    Adding LoadingScreen functionality

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "../Home/Home";
+import LoadingScreen from "../../components/LoadingScreen/LoadingScreen";
 import Login from "../Login/Login";
 import Signup from "../Signup/Signup";
 import Welcome_New from "../Welcome/Welcome_New/Welcome_New";
 import Welcome_User from "../Welcome/Welcome_User/Welcome_User";
 
 class App extends Component {
+  state = {
+    loading: true,
+  };
+
+  componentDidMount() {
+    demoAsyncCall().then(() => this.setState({ loading: false }));
+  }
+
   render() {
-    return (
+    return this.state.loading ? (
+      <LoadingScreen />
+    ) : (
       <div>
         <Router>
           <Switch>
@@ -25,6 +36,10 @@ class App extends Component {
       </div>
     );
   }
+}
+
+function demoAsyncCall() {
+  return new Promise((resolve) => setTimeout(() => resolve(), 3000));
 }
 
 export default App;
